18bce8074f7f0e91c80566eb3f796dba0f02f14d
Jara555 <jaralinders@gmail.com>
2018-04-12 13:24:23 +0200

    Version 1

---
+++ test.py
@@ -0,0 +1,29 @@
+
+class aminoAcid(object):
+
+    def __init__(self, type, x, y):
+
+        self.type = type
+        self.x = x
+        self.y = y
+
+        self.prev = y - 1
+        self.nex = y + 1
+
+    def __str__(self):
+        return "type = "
+
+def main():
+
+    H1 = aminoAcid('H', 0, 0)
+
+    print(H1)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
