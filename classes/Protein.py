8f7a8e8926ff12b3bca1045a170501e700575ca6
Jara555 <jaralinders@gmail.com>
2018-04-17 09:58:39 +0200

    Separate classes

--- classes/Protein.py
+++ classes/Protein.py
@@ -1,4 +1,7 @@
-# the Protein class has AminoAcid as inner class and reflects a list of aminoacids
+# the Protein class reflects a list of aminoacids
+from classes.AminoAcid import AminoAcid
+
+
 class Protein(object):
 
     # input should be a string reflecting the protein (e.g. "HPHHHPHH")
@@ -17,36 +20,11 @@ class Protein(object):
         for aa_index in range(length_protein):
 
             # create aminoacid of class AminoAcid and append to protein list
-            aminoacid = self.AminoAcid(self.protein_string[aa_index], x, y)
+            aminoacid = AminoAcid(self.protein_string[aa_index], x, y)
             self.protein_list.append(aminoacid)
 
             # increment y coordinate with 1 for next aminoacid
             y += 1
-
-    # the AminoAcid class sets up the properties of each aminoacid
-    class AminoAcid(object):
-
-        # uses a char (H/P) and coordinates as input
-        def __init__(self, type, x, y):
-
-            # set type and type specific code
-            self.type = type
-            if self.type == 'H':
-                self.code = 1
-            elif self.type == 'P':
-                self.code = 2
-
-            # set coordinates
-            self.x = x
-            self.y = y
-
-            # link to previous and next aminoacid
-            if self.y != 0:
-                self.prev = y - 1
-            else:
-                self.prev = None
-
-            self.nex = y + 1
 
     # print method
     def __str__(self):
