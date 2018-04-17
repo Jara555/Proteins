1ec7abb56749c550a38a8d84e8cef71973dfa663
Jara555 <jaralinders@gmail.com>
2018-04-17 14:10:12 +0200

    New structure

--- functions/Ufold.py
+++ functions/Ufold.py
@@ -1,4 +1,34 @@
-
 def Ufold(protein):
+    """ Creates folding pattern """
 
+    size = len(protein)
+    patternFold = []
 
+    if size % 2 == 0:
+        fold1 = (size/2) - 1
+        fold2 = size/2
+
+    # odd = []
+    # even = []
+    #
+    # for i in range(size):
+    #     if protein[i] == 1:
+    #         if i % 2 == 0:
+    #             even.append(i)
+    #         else:
+    #             odd.append(i)
+
+    patternFold.append("0")
+
+    for i in range(size - 1):
+        if i == fold1:
+            patternFold.append("-Y")
+        elif i == fold2:
+            patternFold.append("-X")
+        else:
+            patternFold.append("0")
+
+    print(patternFold)
+
+if _name_ == "_main_":
+    main()
