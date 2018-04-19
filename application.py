ef3db3614c891759b60fbaa73d65071552f8f87a
ABra1993 <abrands1993@gmail.com>
2018-04-19 13:50:37 +0200

    Stability folded protein

--- application.py
+++ application.py
@@ -2,28 +2,29 @@ from classes.Grid import Grid
 from classes.Protein import Protein
 from functions.Ufold import Ufold
 from functions.foldprotein import foldprotein
+from functions.stability import stability
 
 def main():
     """ Implements all algorithms in order to most efficiently fold a protein """
 
     # open protein text file
-    with open('data/protein1.txt', 'r') as file:
+    with open('data/protein2.txt', 'r') as file:
         protein_string = file.read()
 
     # create protein of class Protein
     protein = Protein(protein_string)
 
     # create grid of class Grid
-    grid = Grid(len(protein_string))
+    #grid = Grid(len(protein_string))
 
     # place protein in grid
-    grid.placeProtein(protein.protein_list)
+    #grid.placeProtein(protein.protein_list)
 
     # print initial protein and grid
     print()
     print(protein)
     print()
-    grid.printGrid()
+    #grid.printGrid()
     print()
 
     # get folding pattern for folding type Ufold
@@ -33,12 +34,16 @@ def main():
     folded_protein = foldprotein(protein.protein_list, folding_pattern)
 
     # initiate new grid and place folded protein in it
-    grid2 = Grid(len(protein_string))
-    grid2.placeProtein(folded_protein)
+    #grid2 = Grid(len(protein_string))
+    #grid2.placeProtein(folded_protein)
 
     print()
-    grid2.printGrid()
+    #grid2.printGrid()
     print()
+
+    # determine stability protein
+    stabilityProtein = stability(protein.protein_list)
+    print(stabilityProtein)
 
 
 if __name__ == "__main__":
--- functions/stability.py
+++ functions/stability.py
@@ -1,20 +1,26 @@
 def stability(protein):
+    """ Determines stability (number of H-bonds) of folded protein """
 
+    # initializes params
     x = []
     y = []
+    coordinates = []
     score = 0
     orientation = [0, 1, 0, -1, 1, 0, -1, 0]
 
+    # stores x and y coordinates of aminoacids with type "H"
+
+    # ... as integer
     for i in range(len(protein)):
         if protein[i].type == "H":
             x.append(protein[i].x)
             y.append(protein[i].y)
 
-    coordinates = []
-
+    # ... as string
     for i in range(len(x)):
         coordinates.append(str(x[i]) + str(y[i]))
 
+    # loops over aminoacids with type "H" and determines number of H-bonds
     for i in range(len(x)):
 
         for k in range(4):
@@ -32,4 +38,5 @@ def stability(protein):
                 if xy in coordinates and coordinates[i - 1] != xy and coordinates[i + 1] != xy:
                     score = score - 1
 
+    # returns stability
     return score / 2
