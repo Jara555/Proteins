0c88b42673709abba358338a2aa0aff32f785435
Marrit <marritleenstra@hotmail.com>
2018-04-23 11:45:23 +0200

    running the application

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ def main():
     """ Implements all algorithms in order to most efficiently fold a protein """
 
     # open protein text file
-    with open('data/protein1.txt', 'r') as file:
+    with open('data/protein2.txt', 'r') as file:
         protein_string = file.read()
 
     # create protein of class Protein
@@ -44,12 +44,13 @@ def main():
     print(stability(Ufolded_protein))
 
     # test for multipleUfold
-    folding_patterns = multipleUfold(protein_string)
-    folded_protein1 = foldprotein(protein.protein_list, folding_patterns[0])
-    visualize(folded_protein1)
-    folded_protein2 = foldprotein(protein.protein_list, folding_patterns[1])
-
-    visualize(folded_protein2)
+    multipleUfold(protein_string)
+    # folding_patterns = multipleUfold(protein_string)
+    # folded_protein1 = foldprotein(protein.protein_list, folding_patterns[0])
+    # visualize(folded_protein1)
+    # folded_protein2 = foldprotein(protein.protein_list, folding_patterns[1])
+    #
+    # visualize(folded_protein2)
 
     # Random fold: randomize and visualize protein folding patterns
     for i in range(1):
@@ -62,7 +63,7 @@ def main():
         visualize(Rfolded_protein)
 
         # print stability
-        #print(stability(Rfolded_protein))
+        print(stability(Rfolded_protein))
 
         # TODO: update figures instead of open new figures
         plt.show(block=False)
