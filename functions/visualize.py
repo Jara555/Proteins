34843521b75ab6a63867b33f4afa14c240e11de0
Jara555 <jaralinders@gmail.com>
2018-04-23 12:31:24 +0200

    Perfect plots

--- functions/visualize.py
+++ functions/visualize.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
 
 
-def visualize(protein):
+def visualize(protein, name):
     """ Prints the protein in scatter plot with lines"""
+
+    size = len(protein)
 
     print()
     print('Protein coordinates:')
@@ -11,22 +15,40 @@ def visualize(protein):
     y = []
     color = []
 
+    # put x and y coordinates of aminoacid in x and y lists
     for aminoacid in protein:
         x.append(aminoacid.x)
         y.append(aminoacid.y)
+
+        # creates color list for H = red and P = blue
         if aminoacid.type == 'H':
             color.append('red')
         else:
             color.append('blue')
 
+    # print coordinates in terminal
     print(x)
     print(y)
     print()
 
-    plt.plot(x, y, 'C3', zorder=1, lw=3, color='black')
-    plt.scatter(x, y, s=120, zorder=2, color=color)
-    plt.title('Protein')
+    # scatter plot with line
+    plt.plot(x, y, 'C3', zorder=1, lw=2, color='black')
+    plt.scatter(x, y, s=50, zorder=2, color=color)
+    plt.title('Protein: ' + name)
     plt.tight_layout()
-    #plt.legend()
+    plt.axis('scaled')
+
+    plt.ylim(-size, size)
+    plt.xlim(-size, size)
+
+    plt.xticks(np.arange(-size, size + 1, 1.0))
+    plt.yticks(np.arange(-size, size + 1, 1.0))
+
+    plt.axhline(0, linestyle='--', color='gray', linewidth=0.5)  # horizontal lines
+    plt.axvline(0, linestyle='--', color='gray', linewidth=0.5)  # vertical lines
+
+    hydrofoob = mpatches.Patch(color='red', label='H')
+    polair = mpatches.Patch(color='blue', label='P')
+    plt.legend(handles=[hydrofoob, polair])
 
     plt.show()
