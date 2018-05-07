8101443bf313e5018eb831a1f16c466fc9d1bc3d
Jara555 <jaralinders@gmail.com>
2018-05-07 13:47:45 +0200

    astar

---
+++ classes/algorithm/testAstar.py
@@ -0,0 +1,17 @@
+'''
+
+    node = patroon met 1 vouws opties
+    parent = vorige vouw state
+    children = alle mogelijke nodes (alle mogelijke 1 staps vouw opties)
+    queue = meest promesing patronen staan bovenaan
+    start value = stabiliteit van 0
+    eind value = stabiliteit van -9
+    value = huidige stabiliteit
+    cost = kosten van een vouw (bijv 0.1)
+    distance = value - eind value
+    path = alle vouwpatronen in 1 vouws stapjes die die is afgelopen
+
+
+
+
+'''
