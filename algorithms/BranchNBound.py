224e845409814ea678b67b8ab112bcbb32a83170
Marrit <marritleenstra@hotmail.com>
2018-05-22 12:00:56 +0200

    updated pruning with correction for max bonds debugged removeDoubleBonds updated findBonds for CH-bonds

--- algorithms/BranchNBound.py
+++ algorithms/BranchNBound.py
@@ -6,26 +6,30 @@ class BranchNBound(DepthFirst):
     Implements Branch 'N Bound algorithms in order to efficiently fold a protein """
 
     def __init__(self, protein, writeCsv, maxIterations=None):
-        """
-        Set and initiate all properties.
+        """ Set and initiate all properties.
 
         :param protein: protein to be fold
         :param writeCsv: writes solutions to .csv file when ON
         :param maxIterations: stop after maxIterations
         """
 
-        #initialize input varialbes
+        #initialize input variables
         self.maxIterations = maxIterations
 
         # set class properties
         DepthFirst.__init__(self, protein, writeCsv)
         self.name = "BranchNBound"
 
-        self.protein.findbonds()
+        self.protein.findBonds()
 
     def pruneBranchNBound(self, k):
+        """ use pruning
 
-        # go to next orientation
+        :param k: the aminoacid currently being placed
+        :return: True if branch is pruned, False if not pruned
+        """
+
+        # prune
         if self.protein.prune(k, self.bestStability):
             self.pruneCount += 1
             return True
